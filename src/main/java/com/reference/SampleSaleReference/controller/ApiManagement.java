2ad51bfc64821bca5315a8eff0c223c9070e603e
venkataramu222@gmail.com <venkataramu222@gmail.com>
2019-04-18 08:45:47 +0530

    commited flash sale changes

--- src/main/java/com/reference/SampleSaleReference/controller/ApiManagement.java
+++ src/main/java/com/reference/SampleSaleReference/controller/ApiManagement.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 
+import com.reference.SampleSaleReference.model.Product;
 import com.reference.SampleSaleReference.model.User;
+import com.reference.SampleSaleReference.repository.ProductRepository;
 import com.reference.SampleSaleReference.repository.UserRespository;
 
 @RestController
@@ -23,6 +25,15 @@ public class ApiManagement {
 	
 	@Autowired
 	UserRespository userRepository;
+	
+	@Autowired 
+	ProductRepository productRepository;
+	
+	@PostMapping("admin/salecount")
+	public Product postSaleCountOfProduct(@Valid @RequestBody Product product) {
+		return productRepository.save(product);
+		
+	}
 	
 	@GetMapping("/users")
 	public List<User> getAllUsers() {
@@ -34,23 +45,40 @@ public class ApiManagement {
 		return userRepository.save(user);
 	}
 	
-	@PostMapping("users/{id}/purchase/sale")
+	@PostMapping("users/{id}/products/{productId}/sale")
 	public String purchaseOrderonSale(
-			@PathVariable(value="id") Long userId) throws Exception {
-		
-		String message = null;
-		User user = userRepository.findById(userId).get();
-		if(user == null) throw new RestClientException("Not a Valid user");
-		
-		if(noOfItemsInSale > 0) {
-			message = "Congrats Your order Successfully Placed. "
-					+ "Your Item will be dispatched to this address "+ user.getAddress();
-			noOfItemsInSale--;
-		} else {
-			message = "Sorry Sale is Over";
+			@PathVariable(value="id") Long userId,
+			@PathVariable(value="productId") Long productId) throws Exception {
+		try {
+			if(userId > 0) {
+				String message = null;
+				User user = userRepository.findById(userId).get();
+				if(user == null) throw new RestClientException("You are not a Valid user");
+				
+				Product product = productRepository.findById(productId).get();
+				if(product == null)
+					throw new RestClientException("Given Product not available in Sale");
+
+				if(product.getSaleCount() > 0) {
+					message = "Congrats Your order Successfully Placed. "
+							+ "Your Item will be dispatched to this address "+ user.getAddress();
+					noOfItemsInSale--;
+					int value = product.getSaleCount();
+					value = value-1;
+					product.setSaleCount(value);
+					productRepository.save(product);
+					
+				} else {
+					message = "Sorry Sale is Over";
+				}
+
+				return message;
+			}
+
+			throw new RestClientException("User Id must be greater than Zero");
+		} catch(Exception e) {
+			throw new Exception();
 		}
-		
-		return message;
 		
 	}
 
--- src/main/resources/sql/create_schema.sql
+++ src/main/resources/sql/create_schema.sql
@@ -7,3 +7,8 @@ name varchar(100) NOT NULL,
 email varchar(100) NOT NULL, 
 phone BIGINT NOT NULL,
 address varchar(250));
+
+DROP TABLE IF EXISTS sample.products;
+CREATE TABLE sample.products(id BIGINT NOT NULL PRIMARY KEY,
+name varchar(20) NOT NULL,
+sale_count INT NOT NULL); 
