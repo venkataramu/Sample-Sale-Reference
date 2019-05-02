fc9d433502c0b7287d2892220872917b53f4f7eb
venkataramu222@gmail.com <venkataramu222@gmail.com>
2019-05-02 12:28:48 +0530

    #Recent Changes

--- src/main/resources/sql/test_entries.sql
+++ src/main/resources/sql/test_entries.sql
@@ -2,3 +2,5 @@ insert into products ( product_id ,name, sale_id) values
 (1111, 'First sale item',1),
 (10076, 'Second item',5),
 (23456, 'Third is another item',11);
+
+insert into sales(sale_id, available_sale_count,sale_start_time, sale_end_time, sale_name) values(10,10,'2019-04-22T15:52:40.410','2019-04-29T15:52:40.410', 'First Sale');
